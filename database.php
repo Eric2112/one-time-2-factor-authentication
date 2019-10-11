791aff9bb8dfe0dc3fc7131fa9373e4410b8c0a1
marina boyero <marinaboyero12@gmail.com>
2019-10-11 13:24:47 +0100

    database check v1

--- database.php
+++ database.php
@@ -1,24 +1,52 @@
 <?php
-// Get values the user enters
-    $username = $_POST['user'];
-    $password = $_POST['pass'];
+session_start();
 
-// To prevent mysql injection
-    $username = stripcslashes($username);
-    $password = stripcslashes($password);
-    $username = mysql_real_escape_string($username);
-    $password = mysql_real_escape_string($password);
+//connect to database
+$db = mysqli_connect("127.0.0.1", "marina", "marinapass", "bank");
 
-// Connect and select databas
-    mysql_connect("localhost", "root", "");
-    mysql_select_db("BANK");
+if(isset($_POST['login_btn'])){
+    $username = mysql_real_escape_string ($_POST['username']);
+    $password = mysql_real_escape_string ($_POST['password']);
 
-// Query the database for user
-    $result = mysql_query("select * from users where username = '$username' and password = '$password'") or die ("Failed to query database" mysql_error());
-    $row = mysql_fetch_array($result);
-    if($row['username'] == $username && $row['password'] == $password){
-            echo "Login succes! Welcome " $row['username'];
+    $sql = "select * from users where username = '$username' and password = '$password'";
+    $result = mysqli_query($db, $sql);
+
+    if(mysqli_num_rows($result)==1){
+        $_SESSION['message'] = "You are now logged in";
+        $_SESSION['username'] = $username;
+        header("location: home.html");
     } else{
-        echo "Failed to login.";
+        $_SESSION['message']= "Username/Password combination incorrect";
     }
+}
     ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Login Page</title>
+    <link rel ="stylesheet" type ="text/css" href ="style.css">
+</head>
+<body>
+    <div class ="header">
+            <h1>Register, login and logout user php mysql</h1>
+    </div>
+
+    <form method = "post" action= "login.php">
+            <table>
+                <tr>
+                <td> Username:</td>
+                <td><input type = "text" id = "username" class = "textInput"></td>
+            </tr>
+            <tr>
+                <td> Password:</td>
+                <td><input type = "password" id = "password" class = "textInput"></td>
+            </tr>
+            <tr>
+            <td></td>
+                <td><input type = "submit" name = "login_btn" value = "Login"></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
