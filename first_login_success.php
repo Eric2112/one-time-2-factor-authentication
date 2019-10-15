8060c8debd02319d218935f7e6c6076e7950ec8d
marina boyero <marinaboyero12@gmail.com>
2019-10-15 19:14:58 +0100

    4- Username and password correct

---
+++ first_login_success.php
@@ -0,0 +1,20 @@
+<?php
+
+// To avoid people entering this page if they haven't logged in first
+	if(!isset($_COOKIE['loggedin'])){
+		header("location:index.php");
+	}
+
+?>
+
+<html>
+	<body>
+		<h1>Enter passcode sent by SMS:</h1>
+		<form action="second_login.php" method="POST">
+			<p>Code:</p><input type="password" name="pin" />
+<br />
+<br />
+<br />
+		<a href="logout.php">Logout</a>
+	</body>
+</html>
