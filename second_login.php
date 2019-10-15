952b2444c8c8ac54fc8a1aa2d96413f97e9341ec
marina boyero <marinaboyero12@gmail.com>
2019-10-15 19:18:21 +0100

    5- PIN check

---
+++ second_login.php
@@ -0,0 +1,46 @@
+<?php
+
+// Acessing data base
+	$username = "root";
+	$password = NULL;
+	$hostname = "localhost";
+
+	$dbhandle = mysqli_connect($hostname, $username, $password) or die("Could not connect to database");
+
+// Data base in which we will have the tables we want to access
+	$selected = mysqli_select_db($dbhandle, "bank");
+
+// Save entered code/PIN into a variable
+	$mycode = $_POST['pin'];
+// Remove slashes from the pin(important when working with databases as we are doing)
+	$mycode = stripslashes($mycode);
+
+// Check pin
+	// We save the username (unique) to who the PIN has been sent for later restore to NULL
+	$query = "SELECT Username FROM users WHERE PIN='$mycode'";
+	$result = mysqli_query($dbhandle, $query);
+	$myuser= mysqli_fetch_array($result);
+	$count = mysqli_num_rows($result);
+	$user= $myuser["Username"];
+
+	if($count==1){
+
+// Pin restored to NULL by checking username, not pin (cuase other users can have same pin)
+		$pin= "UPDATE users SET PIN =NULL WHERE Username='$user'";
+		mysqli_query($dbhandle, $pin);
+
+// Close data base handler
+		mysqli_close($dbhandle);
+
+// Cookie checks if user has logged in, it expires 120 seconds after the log in
+// User has 2 minutes to do all process of verification
+		$seconds = 120 + time();
+		setcookie(loggedin, date("F jS - g:i a"), $seconds);
+// Redirect to page that will request the PIN
+		header("location:second_login_success.php");
+
+	} else{
+// In case the PIN is not correct, redirect to page that requests code
+		header("location:wrong_pin.php");
+	}
+?>
