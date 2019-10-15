1a1989d9a831ea7f9fdd8461b1177d37e14c4d9a
marina boyero <marinaboyero12@gmail.com>
2019-10-15 19:12:34 +0100

    3- Wrong user and password

---
+++ wrong_user_pass.php
@@ -0,0 +1,11 @@
+<html>
+	<body>
+		<h1>Incorrect username and password, please try again:</h1>
+		<form action="first_login.php" method="POST">
+			<p>Username:</p><input type="text" name="user" />
+			<p>Password:</p><input type="password" name="pass" />
+			<br />
+			<input type="submit" value="Login" />
+		</form>
+	</body>
+</html>
