0adf5fd23a9f38b368ed579c40004ed3c76b9f68
Shane Foley <shanefoley@Shanes-MacBook-Pro.local>
2019-10-30 17:35:54 +0000

    Fix error with html and php code

--- index.php
+++ index.php
@@ -1,16 +1,3 @@
-<html>
-	<body>
-		<form action="first_login.php" method="POST">
-			<p>Username:</p><input type="text" name="user" />
-			<p>Password:</p><input type="password" name="pass" />
-			<br />
-			<input type="submit" value="Login" />
-		</form>
-	</body>
-</html>
-
-
-
 <!doctype html>
 <html lang="en">
   <head>
